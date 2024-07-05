2a2cc7897bdeb59ef6103ab43732ebf469c4fbec
AdityaGaur7 <adgaur027@gmail.com>
2024-07-05 23:15:25 +0530

    login done

--- server/index.js
+++ server/index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
-const db  = require('./db/config')
+const db = require('./db/config')
 const cors = require('cors')
 const User = require("./db/User")
 
 app.use(cors())
 app.use(express.json())
-app.post('/register',async(req,res)=>{
- 
-    const user = new User(req.body)
-   let result = await user.save();
-   res.send({result,success:true});
+app.post('/register', async (req, res) => {
 
+    const user = new User(req.body)
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password
+    res.send({ result, success: true });
+
+})
+app.post('/login', async (req, res) => {
+    let user = await User.findOne(req.body).select("-password");
+    if (req.body.email && req.body.password) {
+        if (user) {
+            res.send(user)
+        }
+        else {
+            res.send({ result: 'No User found' });
+    
+        }
+    } else {
+        res.send({ result: 'No User found' });
+
+    }
 })
 
 app.listen(5000);
