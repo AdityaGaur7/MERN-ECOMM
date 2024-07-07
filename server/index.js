f4a75001eef655c43add7f8197ee67c3a863339b
AdityaGaur7 <adgaur027@gmail.com>
2024-07-07 21:44:24 +0530

    JWT integrated

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const db = require('./db/config')
 const cors = require('cors')
 const User = require("./db/User")
 const Product = require("./db/product")
-
+const jwt = require('jsonwebtoken');
+const jwtkey = 'ecom';
 app.use(cors())
 app.use(express.json())
 app.post('/register', async (req, res) => {
@@ -12,19 +13,36 @@ app.post('/register', async (req, res) => {
     const user = new User(req.body)
     let result = await user.save();
     result = result.toObject();
-    delete result.password
-    res.send({ result, success: true });
+    delete result.password;
+    jwt.sign({result},jwtkey,{expiresIn:'2h'},(err,token)=>{
+        if(err){
+            res.send({ result: 'No result found try after sometime', success: false  });
+        }else{
+
+            res.send({result,token, success: true});
+        }
+    
+    })
 
 })
 app.post('/login', async (req, res) => {
     let user = await User.findOne(req.body).select("-password");
     if (req.body.email && req.body.password) {
         if (user) {
-            res.send({user, success: true} )
+            jwt.sign({user},jwtkey,{expiresIn:'2h'},(err,token)=>{
+                if(err){
+                    res.send({ result: 'No User found try after sometime', success: false  });
+                }else{
+
+                    res.send({user,token, success: true});
+                }
+            
+            })
+          
         }
         else {
+           
             res.send({ result: 'No User found', success: false  });
-    
         }
     } else {
         res.send({ result: 'No User found', success: false  });
