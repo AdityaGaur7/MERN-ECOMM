03addeb6c1679cf65ea71542c984a86b07f9d571
AdityaGaur7 <adgaur027@gmail.com>
2024-07-07 21:20:59 +0530

    search api REGEX

--- server/index.js
+++ server/index.js
@@ -79,4 +79,25 @@ app.put("/product/:id",async(req,res)=>{
     }
 })
 
+app.get("/search/:key",async(req,res)=>{
+    let result = await Product.find({
+        "$or":[
+            {name: {$regex:req.params.key}},
+            {company: {$regex:req.params.key}},
+            {category: {$regex:req.params.key}},
+            {price: {$regex:req.params.key}},
+    ]
+    })
+    if(result)
+    res.send({result,success:true});
+else {
+    res.send({result,success:false});
+
+}
+})
+
+
+
+
+
 app.listen(5000);
